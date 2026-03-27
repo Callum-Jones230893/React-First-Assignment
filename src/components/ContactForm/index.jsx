11a3ce65c927ea2432c6f6d26d0644ad5ba41268
Callum Jones <call_jones2@hotmail.com>
2026-03-27 09:48:34 +0100

    updated functionality of mailto

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -2,23 +2,28 @@ import styles from "./contactForm.module.css"
 
 const ContactForm = () => {
   return (
-    <form className={styles.formWrapper}>
+    <form
+      action="mailto:you@example.com"
+      method="POST"
+      encType="text/plain"
+      className={styles.formWrapper}
+    >
       <div className={styles.formDetailWrapper}>
         <label htmlFor="name">
-          <input type="text" id="name" placeholder="Full Name" required/>
+          <input type="text" id="name" name="name" placeholder="Full Name" required/>
         </label>
         <label htmlFor="email">
-          <input type="email" id="email" placeholder="Email Address" required/>
+          <input type="email" id="email" name="email" placeholder="Email Address" required/>
         </label>
         <label htmlFor="phone">
-          <input type="tel" id="phone" placeholder="Phone Number" required/>
+          <input type="tel" id="phone" name="phone" placeholder="Phone Number" required/>
         </label>
         <label htmlFor="postcode">
-          <input type="text" id="postcode" placeholder="Postcode"/>
+          <input type="text" id="postcode" name="postcode" placeholder="Postcode"/>
         </label>
       </div>
       <label htmlFor="message">
-        <textarea id="message" placeholder="Brief description of works"/>
+        <textarea id="message" name="message" placeholder="Brief description of works"/>
       </label>
       <button type="submit">Submit</button>
     </form>
